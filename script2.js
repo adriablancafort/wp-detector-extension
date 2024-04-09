e6ac48809196853642e86b5d23ea85c979575b01
Adria Blancafort <adriablancafort@gmail.com>
2024-04-09 14:07:54 +0200

    New script

---
+++ script2.js
@@ -0,0 +1,17 @@
+// Returns true if wp-content is found in the current page html
+const find_wp_content = () => {
+  return document.documentElement.innerHTML.includes("wp-content");
+};
+
+// Sends a message to the service worker to change the color of the browser extension badge
+const set_badge_color = (detected) => {
+  chrome.runtime.sendMessage({
+    message: "set_badge_color",
+    detected: detected,
+  });
+};
+
+// Runs the function that detects wordpress on the page laod
+const wp_detected = find_wp_content();
+
+set_badge_color(wp_detected);
