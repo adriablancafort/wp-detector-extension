8aa00e1a115169bf077ccfef11f16b7f9c3919e2
Adria Blancafort <adriablancafort@gmail.com>
2024-04-11 18:50:07 +0200

    clean up

--- sw.js
+++ sw.js
@@ -1,15 +1,27 @@
-// Listen for messages
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "set_badge_color") {
-        set_badge_color(request.detected);
-    }
+// Set the badge color
+const set_badge_color = (detected) => {
+  let green = [2, 217, 88, 255];
+  let red = [255, 40, 83, 255];
+  let color = detected ? green : red;
+  chrome.action.setBadgeBackgroundColor({ color: color });
+  chrome.action.setBadgeText({ text: " " });
+};
+
+// Send a message to the content scripts to know if wp was detected
+const sendMessageToTab = (tabId) => {
+  chrome.tabs.sendMessage(tabId, { message: "wp_detected" }, (response) => {
+      set_badge_color(response.detected);
+  });
+};
+
+// Update the badge when the active tab changes
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  sendMessageToTab(activeInfo.tabId);
 });
 
-// Change the color of the browser extension badge
-const set_badge_color = (detected) => {
-    let green = [2, 217, 88, 255];
-    let red = [255, 40, 83, 255];
-    let color = detected ? green : red;
-    chrome.action.setBadgeBackgroundColor({ color: color });
-    chrome.action.setBadgeText({ text: " " });
-};
+// Update the badge when the page is refreshed / changed
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status === "complete") {
+    sendMessageToTab(tabId);
+  }
+});
