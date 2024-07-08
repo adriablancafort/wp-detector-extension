b6f54c1fe7a8de7fcd521511e2d74f3496fb3d53
Adria Blancafort <adriablancafort@gmail.com>
2024-07-08 18:14:19 +0200

    improve wp detection logic

--- content-script.js
+++ content-script.js
@@ -1,11 +1,35 @@
 // Returns true if wp-content is found in the current page html
-const find_wp_content = () => {
-  return document.documentElement.innerHTML.includes("wp-content");
+const findWpContent = () => {
+  const currentHost = window.location.host;
+  const elementsWithUrls = document.querySelectorAll("a[href], img[src], link[href], script[src], iframe[src]");
+
+  for (const element of elementsWithUrls) {
+    let link;
+    switch (element.tagName) {
+      case "A":
+      case "LINK":
+        link = element.getAttribute("href");
+        break;
+      case "IMG":
+      case "SCRIPT":
+      case "IFRAME":
+        link = element.getAttribute("src");
+        break;
+    }
+    
+    if (link && link.includes("/wp-content/")) {
+      const linkHost = new URL(link, window.location.origin).host; // Get origin host in case of relative URLs
+      if (linkHost === currentHost) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 // Listen for a message from the service worker
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === "wp_detected") {
-    sendResponse({ detected: find_wp_content() });
+    sendResponse({ detected: findWpContent() });
   }
 });
